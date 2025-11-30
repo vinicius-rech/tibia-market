91ea169b23f7f5c6901647868cdea7783e94cde6
Vinicius Rech <vinicius.rech.dev@gmail.com>
2025-11-30 04:16:59 -0300

    header improvements

--- app/components/AppHero.vue
+++ app/components/AppHero.vue
@@ -34,53 +34,54 @@ const activeLocale = computed(() =>
 
 <template>
   <header class="hero">
-    <div class="hero__left">
-      <p class="eyebrow">
-        <img class="logo-mark" src="/logo.png" alt="TMD" />
-        {{ messages.common.appName }}
-      </p>
-    </div>
-    <div class="hero__actions">
-      <div class="hero__lang">
-        <p class="hero__lang-label">{{ t("hero.language") }}</p>
-        <div class="hero__lang-select-wrap">
-          <select v-model="selectedLocale" class="hero__lang-select">
-            <option
-              v-for="option in localeOptions"
-              :key="option.code"
-              :value="option.code"
-            >
-              {{ option.flag }} {{ option.label }}
+    <div class="header__container">
+      <div class="hero__left">
+        <p class="eyebrow">
+          <img class="logo-mark" src="/logo.png" alt="TMD" />
+          {{ messages.common.appName }}
+        </p>
+      </div>
+      <div class="hero__actions">
+        <div class="hero__filter">
+          <select id="globalFilter" v-model="globalFilter">
+            <option value="">{{ messages.common.allItems }}</option>
+            <option v-for="item in items" :key="item" :value="item">
+              {{ item }}
             </option>
           </select>
         </div>
+        <button class="primary" type="button" @click="emit('new-trade')">
+          {{ t("hero.newTrade") }}
+        </button>
+        <button class="ghost" type="button" @click="emit('open-fee')">
+          {{ t("hero.fees") }}
+        </button>
+        <button class="ghost" type="button" @click="emit('open-backup')">
+          {{ t("hero.backup") }}
+        </button>
+        <a
+            class="hero__github"
+            href="https://github.com/vinicius-rech/tibia-market"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          <UIcon name="i-simple-icons-github" class="hero__github-icon" />
+          {{ messages.common.github }}
+        </a>
+        <div class="hero__lang">
+          <div class="hero__lang-select-wrap">
+            <select v-model="selectedLocale" class="hero__lang-select">
+              <option
+                  v-for="option in localeOptions"
+                  :key="option.code"
+                  :value="option.code"
+              >
+                {{ option.flag }} {{ option.label }}
+              </option>
+            </select>
+          </div>
+        </div>
       </div>
-      <div class="hero__filter">
-        <select id="globalFilter" v-model="globalFilter">
-          <option value="">{{ messages.common.allItems }}</option>
-          <option v-for="item in items" :key="item" :value="item">
-            {{ item }}
-          </option>
-        </select>
-      </div>
-      <button class="primary" type="button" @click="emit('new-trade')">
-        {{ t("hero.newTrade") }}
-      </button>
-      <button class="ghost" type="button" @click="emit('open-fee')">
-        {{ t("hero.fees") }}
-      </button>
-      <button class="ghost" type="button" @click="emit('open-backup')">
-        {{ t("hero.backup") }}
-      </button>
-      <a
-        class="hero__github"
-        href="https://github.com/vinicius-rech/tibia-market"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <UIcon name="i-simple-icons-github" class="hero__github-icon" />
-        {{ messages.common.github }}
-      </a>
     </div>
   </header>
 </template>
